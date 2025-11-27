8d2758f674251ef8bc14e401d7be1dda43a477e6
Kess <talhasaidbsr@gmail.com>
2025-11-27 17:56:42 +0300

    #2.4

--- urundetay.php
+++ urundetay.php
@@ -448,31 +448,8 @@
     <script src="assets/scripts/script.js"></script>
 
     <script>
-        const modal = document.getElementById('modal');
-        const modalImg = document.getElementById('modal-img');
-        const closeBtn = document.getElementById('close');
         const buttons = document.querySelectorAll('#button-image');
 
-        buttons.forEach(button => {
-            button.addEventListener('click', () => {
-                modal.style.display = 'flex';          // modal açılır
-                modalImg.src = button.querySelector('img').src; // img kaynağı büyük resme gelir
-                modalImg.style.transform = 'scale(0.5)';
-                setTimeout(() => modalImg.style.transform = 'scale(1)', 10); // hafif animasyon
-            });
-        });
-
-        // Kapatma butonuna tıklayınca modal kapanır
-        closeBtn.addEventListener('click', () => {
-            modal.style.display = 'none';
-        });
-
-        // Modal dışında tıklayınca da kapanmasını isteyebilirsin
-        modal.addEventListener('click', (e) => {
-            if (e.target === modal) {
-                modal.style.display = 'none';
-            }
-        });
 
         const mainImg = document.getElementById('main-img');
         const roofImg = document.querySelectorAll('#roof-img');
