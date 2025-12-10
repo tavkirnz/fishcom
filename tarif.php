247466cc841b64adcf92c6036f842fd9a384a6f8
tavkirnz <tavkirnz@gmail.com>
2025-12-10 17:59:35 +0300

    tamam_said

--- tarif.php
+++ tarif.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
     <?php include 'includes/head-links.php' ?>
+    <title>Denizev | Tarif</title>
+    <link rel="stylesheet" href="assets/css/tarif.css">
 </head>
+
 <body>
     <?php include "includes/header.php" ?>
     <?php
