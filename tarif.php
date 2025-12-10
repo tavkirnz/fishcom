d0c234d6eb65d258f421d5ed9e034a8f6a781fda
tavkirnz <tavkirnz@gmail.com>
2025-12-10 16:46:13 +0300

    tarif

---
+++ tarif.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php include 'includes/head-links.php' ?>
+</head>
+<body>
+    <?php include "includes/header.php" ?>
+    <?php
+        $img = "/assets/images/uye.jpg";
+        $title = "Üyelik Formu";
+        $crumb = " / Üyelik Formu";
+        include "includes/breadcrumb-big.php" 
+    ?>
+    <section class="tarif-genel container">
+        <div class="tarif-anadiv">
+            <div class="tarif-baslik">
+                <h2>Özel Soslu Fırında Balik</h2>
+                <span>20/5/2025</span>
+            </div>
+            <figure>
+                <img class="tarif-image" src="assets/images/banner.jpg" alt="">
+            </figure>
+            <div class="tarif-impflex">
+                <div class="kutucuklar">
+                    <div>
+                        <i class="fa-regular fa-clock"></i>
+                    </div>
+                    <div>
+                        <div style="margin-bottom:8px;">
+                            Hazırlanma Süresi
+                        </div>
+                        <div>
+                            20 DK
+                        </div>
+                    </div>
+                </div>
+                <div class="kutucuklar">
+                    <div style="font-size:34px;">
+                        <i class="fa-regular fa-clock"></i>
+                    </div>
+                    <div>
+                        <div style="margin-bottom:8px;">
+                            Pişirme Süresi
+                        </div>
+                        <div>
+                            55 DK
+                        </div>
+                    </div>
+                </div>
+                <div class="kutucuklar">
+                    <div>
+                        <i class="fa-regular fa-clock"></i>
+                    </div>
+                    <div>
+                        <div style="margin-bottom:8px;">
+                            Porsiyon Sayısı
+                        </div>
+                        <div>
+                            4
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="tarif-ana">
+                <br>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam culpa quos doloribus sunt possimus deserunt vel, dolorem ipsam, in tenetur pariatur laboriosam exercitationem quo error cupiditate eius a. Laboriosam, earum!
+                Fugiat esse ea, nulla praesentium quaerat eligendi quos, at, voluptate minima velit nobis similique quo perferendis atque! Cum perspiciatis debitis, corporis necessitatibus provident ea. Hic cum voluptas esse deserunt nostrum!
+                Doloribus, dolorem voluptatum aliquid odio ratione corrupti magni veniam eius quibusdam est culpa recusandae harum molestiae nobis tempora eligendi quas iure. Id magnam sapiente eos eligendi ipsa. Voluptatum, illum id.
+                Culpa voluptate sequi ut distinctio eius nihil quaerat quisquam repudiandae necessitatibus, doloremque deleniti dolor quis placeat esse blanditiis earum ex, sed, quidem eveniet in facere! Ratione accusamus soluta excepturi expedita!
+                Ipsum odit rem molestias quia velit sed sit inventore facilis porro, vitae, obcaecati nobis totam eum corporis? Quisquam architecto error vel hic? Cumque, ab minima? Laborum voluptas nesciunt incidunt. Dignissimos!
+                <br>
+                <br>
+                Nihil quod corporis corrupti. Odio nostrum, repellendus fuga magni ratione, eum id, officiis tenetur quo error voluptatem aperiam nesciunt deserunt molestiae ad voluptates iusto non rem nemo architecto adipisci vel?
+                Excepturi facilis unde minima enim earum. Minima odio dolor quam repellendus, quaerat totam voluptates blanditiis ipsa, fugit iusto quas culpa est. Corporis quos veritatis voluptatem. Error consequuntur rerum ipsa officia.
+                Recusandae labore incidunt a sunt explicabo iusto consequuntur exercitationem modi voluptas, amet saepe sint fugiat aperiam, numquam hic accusamus molestias assumenda commodi quaerat nulla culpa eos dolores animi. Corrupti, in!
+                Accusantium delectus, illum ut explicabo in voluptas blanditiis omnis. Facere delectus unde porro numquam sed impedit eius dolor reiciendis nemo hic repellendus officia incidunt, nostrum nihil magni aliquam odio est?
+                Veniam doloribus adipisci, magnam in laudantium alias, ea debitis iure reiciendis sed accusamus quas autem omnis delectus beatae quod suscipit sunt dolorum hic minima. Fugit quam accusamus laborum assumenda omnis.
+                Ipsa veniam, dolores eligendi eum hic facere ea nemo exercitationem sit perspiciatis aut ullam natus magni molestiae magnam tempora impedit maxime officia est fugit at earum laboriosam totam. Pariatur, quo.
+                Repellendus aliquid, modi est, excepturi asperiores eveniet, magni itaque illum saepe molestias iusto tempore quaerat numquam amet omnis corrupti dolorum cupiditate necessitatibus mollitia praesentium voluptate dolores unde. Aliquam, atque vel?
+                Minus tempore quae esse. Dicta perspiciatis excepturi nesciunt modi maxime neque perferendis soluta, explicabo iure praesentium consectetur vel autem qui, ex ipsam? Libero pariatur hic placeat, officiis ut debitis eos?
+                Dicta voluptatem officiis voluptas animi maiores autem rem sunt esse repellat ipsum qui amet, officia nisi id quibusdam, minus porro assumenda laudantium modi optio maxime error pariatur? Saepe, suscipit accusamus!
+                <br>
+                <br>
+                Et cupiditate quo asperiores, omnis, ducimus suscipit nobis recusandae ad commodi quis rem numquam esse similique? Possimus maxime soluta, quia animi quod saepe cumque, totam unde quam tempora, obcaecati exercitationem.
+                Praesentium itaque ipsum architecto quaerat expedita error, sit quasi, iusto dicta tenetur temporibus harum. Ullam vitae quam dolore aliquam nobis dolor labore quibusdam! Illum voluptate, eos impedit sint commodi id.
+                Possimus doloribus pariatur saepe quos, voluptates voluptate necessitatibus corrupti et atque ducimus quis placeat fugit consequuntur. Quidem eligendi, tempore, mollitia id veniam beatae placeat rem consectetur iusto eveniet perferendis quae.
+                Dicta perferendis voluptas voluptate aut quidem exercitationem doloribus dolor reiciendis accusamus illum facere ipsam mollitia, adipisci rem? Repudiandae quam delectus, odio ex expedita quae ratione, suscipit architecto quo accusamus doloremque?
+                Possimus veritatis suscipit, molestiae quos nostrum tenetur commodi est laborum eveniet aliquam temporibus voluptatibus. Deleniti facilis laudantium quas aut mollitia officiis placeat recusandae rem sunt repellat! Voluptas culpa ipsa autem!
+                Consequatur exercitationem explicabo laudantium sunt culpa debitis ad, distinctio illo minima, delectus optio! Minima harum natus rem doloremque in perferendis tenetur culpa. Exercitationem ducimus fuga quaerat consequuntur maxime consequatur rerum.
+            </div>
+        </div>
+        <section class="bloglist-section">
+            <div class="blog">
+                <div>
+                    <span class="blog-header">Tarifte Kullanılan Ürünler</span>
+                </div>
+                <div class="blog-cizgi">
+                    <div class="blog-cizgi1"></div>
+                    <div class="blog-cizgi2"></div>
+                </div>
+                <div>
+                    <div class="popular-post">
+                        <div>
+                            <img src="assets/images/urun.webp" alt="">
+                        </div>
+                        <div class="popular-text w-full">
+                            <div>
+                                <a href="" class="blog-hover">Balık</a>
+                            </div>
+                            <div class="f ic jb w-3/4">
+                                500TL 
+                                <button id="sepete-ekle">
+                                    Sepete Ekle <i class="fa-solid fa-circle-plus"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="popular-post">
+                        <div>
+                            <img src="assets/images/urun.webp" alt="">
+                        </div>
+                        <div class="popular-text w-full">
+                            <div>
+                                <a href="" class="blog-hover">Balık</a>
+                            </div>
+                            <div class="f ic jb w-3/4">
+                                500TL 
+                                <button id="sepete-ekle">
+                                    Sepete Ekle <i class="fa-solid fa-circle-plus"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="popular-post">
+                        <div>
+                            <img src="assets/images/urun.webp" alt="">
+                        </div>
+                        <div class="popular-text w-full">
+                            <div>
+                                <a href="" class="blog-hover">Balık</a>
+                            </div>
+                            <div class="f ic jb w-3/4">
+                                500TL 
+                                <button id="sepete-ekle">
+                                    Sepete Ekle <i class="fa-solid fa-circle-plus"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <span class="blog-header">Tarifte Kullanılan Ekipmanlar</span>
+                </div>
+                <div class="blog-cizgi">
+                    <div class="blog-cizgi1"></div>
+                    <div class="blog-cizgi2"></div>
+                </div>
+                <div class="ekipmanlar">
+                    <div class="tarif-ekipmanlar">
+                        <img src="assets/images/tik.png" alt="">balık
+                    </div>
+                    <div class="tarif-ekipmanlar">
+                        <img src="assets/images/tik.png" alt="">bıçak
+                    </div>
+                    <div class="tarif-ekipmanlar">
+                        <img src="assets/images/tik.png" alt="">tava
+                    </div>
+                    <div class="tarif-ekipmanlar">
+                        <img src="assets/images/tik.png" alt="">ateş
+                    </div>
+                </div>
+                <div>
+                    <span class="blog-header">Tarifte Kullanılan Ürünler</span>
+                </div>
+                <div class="blog-cizgi">
+                    <div class="blog-cizgi1"></div>
+                    <div class="blog-cizgi2"></div>
+                </div>
+                <div>
+                    <div class="popular-post">
+                        <div>
+                            <img src="assets/images/urun.webp" alt="">
+                        </div>
+                        <div class="popular-text w-full">
+                            <div>
+                                <a href="" class="blog-hover">Özel Soslu Somon</a>
+                            </div>
+                            <div class="">
+                                22 Haziran, 2025
+                            </div>
+                        </div>
+                    </div>
+                    <div class="popular-post">
+                        <div>
+                            <img src="assets/images/urun.webp" alt="">
+                        </div>
+                        <div class="popular-text w-full">
+                            <div>
+                                <a href="" class="blog-hover">Kızartma Somon</a>
+                            </div>
+                            <div class="">
+                                22 Haziran, 2025
+                            </div>
+                        </div>
+                    </div>
+                    <div class="popular-post">
+                        <div>
+                            <img src="assets/images/urun.webp" alt="">
+                        </div>
+                        <div class="popular-text w-full">
+                            <div>
+                                <a href="" class="blog-hover">Balık</a>
+                            </div>
+                            <div class="">
+                                22 Haziran, 2025
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <span class="blog-header">Sosyal Medyada Paylaş</span>
+                </div>
+                <div class="blog-cizgi">
+                    <div class="blog-cizgi1"></div>
+                    <div class="blog-cizgi2"></div>
+                </div>
+            </div>
+        </section>
+    </section>
+    <?php
+        include ("includes/footer.php")
+    ?>
+</body>
+</html>
